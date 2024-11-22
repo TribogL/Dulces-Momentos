90cbc7f5e97ac692a5c08d0f0c164731272b7ba7
Tribog <julix4212@gmail.com>
2024-11-22 07:54:30 -0500

    feat: The model"Rol" was created succesfuly.

---
+++ Models/Rol.cs
@@ -0,0 +1,34 @@
+
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dulces_Momentos.Models;
+
+[Table("rols")]
+public class Rol
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("name")]
+    [MaxLength(250, ErrorMessage = "The name cannot exceed 50 characters")]
+    [Required(ErrorMessage = "The name is required")]
+    public string Name { get; set; }
+    [Column("description")]
+    [MaxLength(255, ErrorMessage = "The description cannot exceed 255 characters")]
+    [Required(ErrorMessage = "The name is required")]
+    public string Description { get; set; }
+
+
+    public Rol()
+    {
+        
+    }
+
+    public Rol(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
